3647d021691b1e3926d2d8029e056fc24526b841
otee042 <onthatile.totwe2@gmail.com>
2023-06-23 14:57:03 +0200

    dwa10

--- DWA-10/script.js
+++ DWA-10/script.js
@@ -5,46 +5,39 @@ const STEP_AMOUNT = 1
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
-const reset = document.querySelector('[data-key="reset"]')
+const reset = document.querySelector('[data-key="reset"]');    //adde new button
+
 
 const subtractHandler = () => {
     const newvalue = parseInt(number.value) - STEP_AMOUNT
-    number.value = newvalue 
-
+    number.value = newvalue
     if (add.disabled === true) {
         add.disabled = false
     }
-
     if (newvalue <= MIN_NUMBER) {
         subtract.disabled = true
     } else {
         subtract.disabled = false
     }
 }
-
 const addHandler = () => {
     const newvalue = parseInt(number.value)  + STEP_AMOUNT
     number.value = newvalue
-
     if (subtract.disabled === true) {
         subtract.disabled = false
     }
-
     if (newvalue >= MAX_NUMBER) {
         add.disabled = true
     }
-        
 }
 
+//
 const resetHandler = () => {
     number.value = 0;
-    add.disabled = false;
     subtract.disabled = false;
-    showMessage("The counter has been reset");
-  }
-  const message = (message) =>{
-    alert(showMessage)
-  };
+    add.disabled = false;
+    alert('the counter has been reset');
+}
 
 subtract.addEventListener('click', subtractHandler)
 add.addEventListener('click', addHandler)
