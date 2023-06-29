525c6c1b848657d8a5cf83aa11cb69248e83e6d9
otee042 <onthatile.totwe2@gmail.com>
2023-06-29 16:16:13 +0200

    dwa13

--- DWA-13/script.js
+++ DWA-13/script.js
@@ -12,7 +12,40 @@ names.forEach((name, index) => {
 });
 
 //for using map
-const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+const BigProvinces = provinces.map(province => province.toUpperCase());
 
-console.log(uppercaseProvinces);
+console.log(BigProvinces);
+
+//new array
+// const nameLengths = provinces.map(province => province.length);
+// console.log(nameLengths);
+
+// const sortedProvinces = provinces.sort();
+// console.log(sortedProvinces);
+
+//Using toSorted method
+const sortProvinces = provinces.sort();
+console.log(sortProvinces);
+
+//Using Filter method
+const filterProvinces = provinces.filter(province => !province.includes('Cape'));
+const remainingCount = filterProvinces.length;
+
+console.log(remainingCount);
+
+//for names containing S character
+const containsS = names.map(name => name.includes('S'));
+console.log(containsS);
+
+const containsSUsingSome = names.map(name => name.split('').some(char => char === 'S'));
+console.log(containsSUsingSome);
+
+//for object
+const nameProvinceObj = names.reduce((obj, name, index) => {
+  obj[name] = provinces[index];
+  return obj;
+}, {});
+
+console.log(nameProvinceObj);
+
 
